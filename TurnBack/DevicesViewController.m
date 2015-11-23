1f74dd186d74ea354470048ad91902eb8d1c0282
ezefranca <ezequiel.ifsp@gmail.com>
2015-11-23 02:27:57 -0200

    adicionada lib bluetooth

--- TurnBack/DevicesViewController.m
+++ TurnBack/DevicesViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "DevicesViewController.h"
+#import "LGBluetooth.h"
 
 @interface DevicesViewController ()
 
@@ -16,11 +17,82 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // Initialization of CentralManager
+    [LGCentralManager sharedInstance];
+    // Scaning 4 seconds for peripherals
+    [[LGCentralManager sharedInstance] scanForPeripheralsByInterval:4
+                                                         completion:^(NSArray *peripherals)
+     {
+         // If we found any peripherals sending to test
+         if (peripherals.count) {
+             [self testPeripheral:peripherals[0]];
+         }
+     }];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - TableView Delegate and Datasource
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [[UITableViewCell alloc]init];
+    return cell;
+}
+
+#pragma mark - Bluetooth Methods
+- (IBAction)testPressed:(UIButton *)sender
+{
+
+}
+
+- (void)testPeripheral:(LGPeripheral *)peripheral
+{
+    // First of all connecting to peripheral
+    [peripheral connectWithCompletion:^(NSError *error) {
+        // Discovering services of peripheral
+        [peripheral discoverServicesWithCompletion:^(NSArray *services, NSError *error) {
+            for (LGService *service in services) {
+                // Finding out our service
+                if ([service.UUIDString isEqualToString:@"5ec0"]) {
+                    // Discovering characteristics of our service
+                    [service discoverCharacteristicsWithCompletion:^(NSArray *characteristics, NSError *error) {
+                        // We need to count down completed operations for disconnecting
+                        __block int i = 0;
+                        for (LGCharacteristic *charact in characteristics) {
+                            // cef9 is a writabble characteristic, lets test writting
+                            if ([charact.UUIDString isEqualToString:@"cef9"]) {
+                                [charact writeByte:0xFF completion:^(NSError *error) {
+                                    if (++i == 3) {
+                                        // finnally disconnecting
+                                        [peripheral disconnectWithCompletion:nil];
+                                    }
+                                }];
+                            } else {
+                                // Other characteristics are readonly, testing read
+                                [charact readValueWithBlock:^(NSData *data, NSError *error) {
+                                    if (++i == 3) {
+                                        // finnally disconnecting
+                                        [peripheral disconnectWithCompletion:nil];
+                                    }
+                                }];
+                            }
+                        }
+                    }];
+                }
+            }
+        }];
+    }];
+}
+
 @end
--- TurnBack/LoginViewController.m
+++ TurnBack/LoginViewController.m
@@ -7,21 +7,32 @@
 //
 
 #import "LoginViewController.h"
+#import "DevicesViewController.h"
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
 
+
 @interface LoginViewController () <FBSDKLoginButtonDelegate>
+@property (strong, nonatomic) IBOutlet UIView *facebookButtonFrame;
 @end
 
+
+
 @implementation LoginViewController
+@synthesize facebookButtonFrame;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] initWithFrame:_buttonView.frame];
-    loginButton.delegate = self;
-    loginButton.readPermissions = @[@"public_profile", @"email", @"user_friends", @"user_likes", @"user_location", @"user_tagged_places"];
-    [self.view addSubview:loginButton];
-    // Do any additional setup after loading the view, typically from a nib.
+//    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+//    // Optional: Place the button in the center of your view.
+//    loginButton.center = self.view.center;
+//    loginButton.delegate = self;
+//    loginButton.readPermissions = @[@"public_profile", @"email", @"user_friends", @"user_likes", @"user_location", @"user_tagged_places"];
+//    [self.view addSubview:loginButton];
+    
+    
+    DevicesViewController *viewController = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"devicesVC"];
+    [self presentViewController:viewController animated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,8 +47,9 @@
 }
 
 -(void)loginButton:(FBSDKLoginButton *)loginButton didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result error:(NSError *)error{
-    
     NSLog(@"Login ok: %@", result.grantedPermissions);
+    DevicesViewController *viewController = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"devicesVC"];
+    [self presentViewController:viewController animated:YES completion:nil];
 }
 
 @end
